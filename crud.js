cab0b53e0f7e9f70392e2176f31d076ad928b5df
Topi Santakivi <topi.santakivi@gmail.com>
2017-07-23 23:48:58 +0300

    Implement basic Swagger generation

--- crud.js
+++ crud.js
@@ -1,16 +1,45 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 function tableUrl(basePath, table) {
   return `${basePath}/${table.tableName.toLowerCase()}`;
 }
 
+function getSwaggerSpec(apiDoc) {
+  const swaggerInfo = {
+    swaggerDefinition: {
+        info: {
+          title: 'Basic REST API',
+          version: '1.0.0'
+        },
+        produces: ['application/json'],
+        consumes: ['application/json']
+      },
+      apis: [
+        /*'lib/routes/*.js'*/
+      ]
+  };
+
+  const spec = swaggerJSDoc(swaggerInfo);
+  spec.paths = apiDoc;
+
+  return spec;
+}
+
 module.exports = {
-  generate: (app, basePath, tables, apiInfo) => {
+  generate: (app, basePath, tables) => {
+
+    const apiDoc = {};
 
     tables.forEach((table) => {
+      const tableDoc = {};
+
+      const tableApiPath = tableUrl(basePath, table);
+
       // Create
-      app.post(tableUrl(basePath, table), function* (req, res) {
+      app.post(tableApiPath, function* (req, res) {
         const item =
           yield table
             .query()
@@ -18,9 +47,40 @@ module.exports = {
 
         res.send(item);
       });
+      /**
+         * @swagger
+         * /users:
+         *   post:
+         *     description: Returns users
+         *     produces:
+         *       - application/json
+         *     parameters:
+         *       - name: user
+         *         description: User object
+         *         in:  body
+         *         required: true
+         *         type: string
+         *         schema:
+         *           $ref: '#/definitions/NewUser'
+         *     responses:
+         *       200:
+         *         description: users
+         *         schema:
+         *           $ref: '#/definitions/User'
+         */      
+
+      tableDoc.post = {
+        description: `Creates a new ${table.tableName}`,
+        parameters: [{
+          name: table.tableName.toLowerCase(),
+          in: 'body',
+          required: true,
+          schema: table.jsonSchema
+        }]
+      };
 
       // Get all
-      app.get(tableUrl(basePath, table), function* (req, res) {
+      app.get(tableApiPath, function* (req, res) {
         const items =
           yield table
             .query();
@@ -28,8 +88,12 @@ module.exports = {
         res.send(items);
       });
 
+      tableDoc.get = {
+        description: `Gets a list of ${table.tableName}`,
+      };      
+
       // Get one
-      app.get(tableUrl(basePath, table) + '/:id', function* (req, res) {
+      app.get(tableApiPath + '/:id', function* (req, res) {
         const item =
           yield table
             .query()
@@ -38,8 +102,18 @@ module.exports = {
         res.send(item);
       });
 
+      tableDoc.get = {
+        description: `Gets a single ${table.tableName}`,
+        parameters: [{
+          name: 'id',
+          in: 'path',
+          required: true,
+          type: 'integer'
+        }]
+      };   
+
       // Update
-      app.patch(tableUrl(basePath, table) + '/:id', function* (req, res) {
+      app.patch(tableApiPath + '/:id', function* (req, res) {
         const item =
           yield table
             .query()
@@ -48,15 +122,42 @@ module.exports = {
         res.send(item);
       });
 
+      tableDoc.patch = {
+        description: `Updates a ${table.tableName}`,
+        parameters: [{
+          name: 'id',
+          in: 'path',
+          required: true,
+          type: 'integer'
+        }]
+      };      
+
       // Delete
-      app.delete(tableUrl(basePath, table) + '/:id', function* (req, res) {
+      app.delete(tableApiPath, function* (req, res) {
         yield table
           .query()
           .deleteById(req.params.id);
 
         res.send({});
       });
+
+      tableDoc.delete = {
+        description: `Deletes a ${table.tableName}`,
+        parameters: [{
+          name: 'id',
+          in: 'path',
+          required: true,
+          type: 'integer'
+        }]
+      };         
+
+      apiDoc[tableApiPath] = tableDoc;
     });
+
+    // Generate the Swagger documentation
+
+    app.use('/swagger', swaggerUi.serve, swaggerUi.setup(getSwaggerSpec(apiDoc)));
+
   }
 };
 
--- models/Movie.js
+++ models/Movie.js
@@ -3,6 +3,7 @@
 const Model = require('objection').Model;
 
 class Movie extends Model {
+
   // Table name is the only required property.
   static get tableName() {
     return 'Movie';
